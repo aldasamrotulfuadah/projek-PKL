2315bb26f6a2db69cedaa7c8810f3ba7c273dd53
iskndrio <rioiskandar1219@gmail.com>
2026-04-01 14:13:25 +0700

    bebas

---
+++ .history/app/Http/Controllers/SuratMasukController_20260401120245.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SuratMasuk;
+use Illuminate\Http\Request;
+
+class SuratMasukController extends Controller
+{
+    public function index(){
+        $data = array(
+            'title'   => 'Data Surat Masuk',
+            'menuSuratMasuk'   => 'active',
+            'suratmasuk'   => suratmasuk::orderBy('sifat','asc')->get(),
+        ); 
+        return view('suratm/suratmasuk',$data);
+    }
+    
+    public function create(){
+        $data = array(
+            'title'   => 'Tambah Data Surat Masuk',
+            'menuSuratMasuk'   => 'active',
+        ); 
+        return view('suratm/create',$data);
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'tanggal_surat'         => 'required|date',
+            'nomor_surat'           => 'required',
+            'diterima_dari'         => 'required',
+            'perihal'               => 'required',
+            'sifat'                 => 'required',
+            'diteruskan_kepada'     => 'required',
+            'dengan_hormat_harap'   => 'required',
+        ],[
+            'tanggal_surat.required'            => 'Tanggal Surat Tidak Boleh Kosong',
+            'nomor_surat.required'              => 'Nomor Surat Tidak Boleh Kosong',
+            'diterima_dari.required'            => 'Tidak Boleh Kosong',
+            'perihal.required'                  => 'Perihal Surat Tidak Boleh Kosong',
+            'sifat.required'                    => 'Sifat Surat Harus Dipilih',
+            'diteruskan_kepada.required'        => 'Disposisi Surat Harus Dipilih',
+            'dengan_hormat_harap.required'      => 'Disposisi Surat Harus Dipilih',
+        ]);
+
+        $suratmasuk = new suratmasuk;
+        $suratmasuk->tanggal_surat              = $request->tanggal_surat;
+        $suratmasuk->nomor_surat                = $request->nomor_surat;
+        $suratmasuk->diterima_dari              = $request->diterima_dari;
+        $suratmasuk->perihal                    = $request->perihal;
+        $suratmasuk->sifat                      = $request->sifat;
+        $suratmasuk->diteruskan_kepada          = $request->diteruskan_kepada;
+        $suratmasuk->dengan_hormat_harap        = $request->dengan_hormat_harap;
+        $suratmasuk->no_wa = $request->no_wa;
+        $suratmasuk->save();
+
+        return redirect()->route('suratmasuk')->with('success','Data Berhasil Ditambahkan');
+    }
+
+    public function edit($id){
+        $data = array(
+            'title'   => 'Edit Data Surat Masuk',
+            'menuSuratMasuk'   => 'active',
+            'suratmasuk' => SuratMasuk::findOrFail($id),
+        ); 
+        return view('suratm/edit',$data);
+    }
+
+     public function update(Request $request, $id){
+        $request->validate([
+            'tanggal_surat'         => 'required|date',
+            'nomor_surat'           => 'required',
+            'diterima_dari'         => 'required',
+            'perihal'               => 'required',
+            'sifat'                 => 'required',
+            'diteruskan_kepada'     => 'required',
+            'dengan_hormat_harap'   => 'required',
+        ],[
+            'tanggal_surat.required'            => 'Tanggal Surat Tidak Boleh Kosong',
+            'nomor_surat.required'              => 'Nomor Surat Tidak Boleh Kosong',
+            'diterima_dari.required'            => 'Tidak Boleh Kosong',
+            'perihal.required'                  => 'Perihal Surat Tidak Boleh Kosong',
+            'sifat.required'                    => 'Sifat Surat Harus Dipilih',
+            'diteruskan_kepada.required'        => 'Disposisi Surat Harus Dipilih',
+            'dengan_hormat_harap.required'      => 'Disposisi Surat Harus Dipilih',
+        ]);
+
+        $suratmasuk = SuratMasuk::findOrFail($id);
+        $suratmasuk->tanggal_surat              = $request->tanggal_surat;
+        $suratmasuk->nomor_surat                = $request->nomor_surat;
+        $suratmasuk->diterima_dari              = $request->diterima_dari;
+        $suratmasuk->perihal                    = $request->perihal;
+        $suratmasuk->sifat                      = $request->sifat;
+        $suratmasuk->diteruskan_kepada          = $request->diteruskan_kepada;
+        $suratmasuk->dengan_hormat_harap        = $request->dengan_hormat_harap;
+        $suratmasuk->no_wa = $request->no_wa;
+        $suratmasuk->save();
+
+        return redirect()->route('suratmasuk')->with('success','Data Berhasil Di Edit');
+    }
+
+    public function destroy($id){
+        $suratmasuk = SuratMasuk::findOrFail($id);
+        $suratmasuk->delete();
+
+        return redirect()->route('suratmasuk')->with('success', 'Data Berhasil Di Hapus');
+    }
+
+        public function kirimWaSurat(Request $request, $id)
+    {
+        $data = SuratMasuk::findOrFail($id);
+        $nomorWA = $data->no_wa;
+
+        $pesan = "📩 *SURAT MASUK*\n\n".
+                "Tanggal Surat: ".$surat->tanggal_surat."\n".
+                "Nomor Surat: ".$surat->nomor_surat."\n".
+                "Diterima Dari: ".$data->diterima_dari."\n".
+                "Perihal: ".$data->perihal;
+                "Sifat: ".$data->sifat;
+                "Dengan Hormat Harap: ".$data->dengan_hormat_harap;
+                "Diteruskan Kepada ".$data->diteruskan_kepada;
+
+        $target = $request->no_wa; // Ambil nomor yang dipilih
+
+       // encode pesan untuk URL
+    $waLink = "https://wa.me/".$nomorWA."?text=".urlencode($pesan);
+
+    // redirect ke WA
+    return redirect()->away($waLink);
+    }
+
+
+}
---
+++ .history/resources/views/suratm/edit.blade_20260401130217.php
@@ -0,0 +1,146 @@
+@extends('layouts/app')
+
+@section('content')
+
+    <h1 class="h3 mb-4 text-gray-800">
+        <i class="fas fa-edit mr-2"></i>
+       {{ $title }} 
+    </h1>
+
+    <div class="card">
+        <div class="card-header">
+                <a href="{{ route('suratmasuk') }}" class="btn btn-sm btn-secondary">
+                    <i class="fas fa-arrow-left mr-2"></i>
+                Kembali
+                </a>
+        </div>
+        <div class="card-body">
+            <form action="{{ route('suratmasukUpdate', $suratmasuk->id) }}" method="post">
+                @csrf
+                <div class="row mb-2">
+                    <div class="col-xl-6 mb-2">
+                        <label class="form-label">
+                            <span class="text-danger">*</span>
+                            Tanggal Surat :
+                        </label>
+                        <input type="date" name="tanggal_surat" class="form-control @error('tanggal_surat') is-invalid @enderror" value="{{ $suratmasuk->tanggal_surat }}">
+                        @error('tanggal_surat')
+                            <small class="text-danger">
+                                {{ $message }}
+                            </small>
+                        @enderror
+                    </div>
+                    <div class="col-xl-6 mb-1">
+                        <label class="form-label">
+                            <span class="text-danger">*</span>
+                            Nomor Surat :
+                        </label>
+                        <input type="text" name="nomor_surat" class="form-control @error('nomor_surat') is-invalid @enderror" value="{{ $suratmasuk->nomor_surat }}">
+                        @error('nomor_surat')
+                            <small class="text-danger">
+                                {{ $message }}
+                            </small>
+                        @enderror
+                    </div>
+                </div>
+                <div class="row mb-2">
+                    <div class="col-xl-6">
+                        <label class="form-label">
+                            <span class="text-danger">*</span>
+                            Diterima Dari :
+                        </label>
+                        <input type="text" name="diterima_dari" class="form-control @error('diterima_dari') is-invalid @enderror"  value="{{$suratmasuk->diterima_dari }}">
+                        @error('diterima_dari')
+                            <small class="text-danger">
+                                {{ $message }}
+                            </small>
+                        @enderror
+                    </div>
+                    <div class="col-xl-6">
+                        <label class="form-label">
+                            <span class="text-danger">*</span>
+                            Perihal :
+                        </label>
+                        <input type="text" name="perihal" class="form-control @error('perihal') is-invalid @enderror"  value="{{ $suratmasuk->perihal }}">
+                        @error('perihal')
+                            <small class="text-danger">
+                                {{ $message }}
+                            </small>
+                        @enderror
+                    </div>
+                </div>
+                <div class="row mb-2">
+                    <div class="col-xl-6 mb-2">
+                        <label class="form-label">
+                            <span class="text-danger">*</span>
+                            Sifat :
+                        </label>
+                        <select name="sifat" class="form-control @error('sifat') is-invalid @enderror">
+                            <option selected disabled>-- Pilih Sifat Surat --</option>
+                            <option value="Sangat Segera" {{ $suratmasuk->sifat == 'Sangat Segera' ? 'selected' : ''}}>Sangat Segera</option>
+                            <option value="Segera" {{ $suratmasuk->sifat == 'Segera' ? 'selected' : ''}}>Segera</option>
+                            <option value="Rahasia"  {{ $suratmasuk->sifat == 'Rahasia' ? 'selected' : ''}}>Rahasia</option>
+                        </select>
+                        @error('sifat')
+                            <small class="text-danger">
+                                {{ $message }}
+                            </small>
+                        @enderror
+                    </div>
+                    <div class="col-xl-6">
+                        <label class="form-label">
+                            <span class="text-danger">*</span>
+                            Diteruskan Kepada :
+                        </label> 
+                        <select name="diteruskan_kepada" class="form-control @error('diteruskan_kepada') is-invalid @enderror">
+                            <option selected disabled>-- Pilih Bidang Terkait --</option>
+                            <option value="Sekretaris" {{ $suratmasuk->diteruskan_kepada == 'Sekretaris' ? 'selected' : ''}}>Sekretaris</option>
+                            <option value="Kabid Ideologi Wawasan Kebangsaan, Ketahanan Eksosbud & Agama" {{ $suratmasuk->diteruskan_kepada == 'Kabid Ideologi Wawasan Kebangsaan, Ketahanan Eksosbud & Agama' ? 'selected' : ''}}>Kabid Ideologi Wawasan Kebangsaan, Ketahanan Eksosbud & Agama</option>
+                            <option value="Kabid Kewaspadaan Nasional & Penanganan Konflik" {{ $suratmasuk->diteruskan_kepada == 'Kabid Kewaspadaan Nasional & Penanganan Konflik' ? 'selected' : ''}}>Kabid Kewaspadaan Nasional & Penanganan Konflik</option>
+                            <option value="Kabid Politik Dalam Negeri & Ormas" {{ $suratmasuk->diteruskan_kepada == 'Kabid Politik Dalam Negeri & Ormas' ? 'selected' : ''}}>Kabid Politik Dalam Negeri & Ormas</option>
+                            <option value="Subid Ideologi Wasbang" {{ $suratmasuk->diteruskan_kepada == 'Subid Ideologi Wasbang' ? 'selected' : ''}}>Subid Ideologi Wasbang</option>
+                            <option value="Subid Ketahanan Eksosbud dan Agama" {{ $suratmasuk->diteruskan_kepada == 'Subid Ketahanan Eksosbud dan Agama' ? 'selected' : ''}}>Subid Ketahanan Eksosbud dan Agama</option>
+                            <option value="Subag Umun dan Kepegawaian" {{ $suratmasuk->diteruskan_kepada == 'Subag Umun dan Kepegawaian' ? 'selected' : ''}}>Subag Umun dan Kepegawaian</option>
+                            <option value="Subag Program Anggaran dan Keuangan" {{ $suratmasuk->diteruskan_kepada == 'Subag Program Anggaran dan Keuangan' ? 'selected' : ''}}>Subag Program Anggaran dan Keuangan</option>
+                        </select>  
+                        @error('diteruskan_kepada')
+                            <small class="text-danger">
+                                {{ $message }}
+                            </small>
+                        @enderror
+                    </div>
+                </div>
+                <div class="row mb-2">
+                    <div class="col-xl-12 mb-2">
+                        <label class="form-label">
+                            <span class="text-danger">*</span>
+                            Dengan Hormat Harap :
+                        </label>
+                        <select name="dengan_hormat_harap" class="form-control @error('dengan_hormat_harap') is-invalid @enderror">
+                            <option selected disabled>-- Pilih --</option>
+                            <option value="Tanggapan dan Saran"{{ $suratmasuk->dengan_hormat_harap == 'Tanggapan dan Saran' ? 'selected' : ''}}>Tanggapan dan Saran</option>
+                            <option value="Proses Lebih Lanjut" {{ $suratmasuk->dengan_hormat_harap == 'Proses Lebih Lanjut' ? 'selected' : ''}}>Proses lebih Lanjut</option>
+                            <option value="Koordinasi/Konsultasi" {{ $suratmasuk->dengan_hormat_harap == 'Koordinasi/Konsultasi' ? 'selected' : ''}}>Koordinasi/konsultasi</option>
+                            <option value="Bahan Pertimbangan"{{ $suratmasuk->dengan_hormat_harap == 'Bahan Pertimbangan' ? 'selected' : ''}}>Bahan pertimbangan</option>
+                            <option value="Dibahas/Dikaji" {{ $suratmasuk->dengan_hormat_harap == 'Dibahas/Dikaji' ? 'selected' : ''}}>Dibahas/Dikaji</option>
+                            <option value="Dibantu" {{ $suratmasuk->dengan_hormat_harap == 'Dibantu' ? 'selected' : ''}}>Dibantu</option>
+                            <option value="Disiapkan Bahan" {{ $suratmasuk->dengan_hormat_harap == 'Disiapkan Bahan' ? 'selected' : ''}}>Disiapkan Bahan</option>
+                            <option value="Mendampingi" {{ $suratmasuk->dengan_hormat_harap == 'Mendampingi' ? 'selected' : ''}}>Mendampingi</option>
+                            <option value="Menghadiri" {{ $suratmasuk->dengan_hormat_harap == 'Menghadiri' ? 'selected' : ''}}>Menghadiri</option>
+                            <option value="Mewakili" {{ $suratmasuk->dengan_hormat_harap == 'Mewakili' ? 'selected' : ''}}>Mewakili</option>
+                            <option value="UMM" {{ $suratmasuk->dengan_hormat_harap == 'UMM' ? 'selected' : ''}}>UMM</option>
+                            <option value="UM" {{ $suratmasuk->dengan_hormat_harap == 'UM' ? 'selected' : ''}}>UM</option>
+                    </select>
+                <div>
+                    <button type="submit" class="btn btn-sm btn-secondary">
+                        <i class="fas fa-edit mr-2"></i>
+                        Edit
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+    
+
+
+@endsection
