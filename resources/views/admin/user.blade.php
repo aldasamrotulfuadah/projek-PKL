8f88b06c8285f6ce9b5983366297dd51cdd106a1
aldasamrotulfuadah <alda.samrotul2007@smkn4-tsm.sch.id>
2026-04-06 13:37:01 +0700

    Update user.blade.php

--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -19,53 +19,53 @@
         </div>
         <div class="card-body">
             <div class="table-responsive">
-                                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                        <thead class="bg-danger text-white">
-                                            <tr class="text-center">
-                                                <th>No</th>
-                                                <th>Nama</th>
-                                                <th>Email</th>
-                                                <th>Jabatan</th>
-                                                <th>
-                                                    <i class="fas fa-cog"></i>
-                                                </th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach ($user as $item)
-                                            <tr>
-                                                <td class="text-center">{{ $loop->iteration }}</td>
-                                                <td>{{ $item->nama }}</td>
-                                                <td class="text-center">
-                                                    <span class="badge badge-secondary">
-                                                        {{ $item->email }}
-                                                    </span>
-                                                </td>
-                                                <td class="text-center">
-                                                    @if ($item->Jabatan == 'Admin')
-                                                    <span class="badge badge-secondary">
-                                                        {{ $item->Jabatan }}
-                                                    </span>
-                                                    @else
-                                                    <span class="badge badge-light">
-                                                        {{ $item->Jabatan }}
-                                                    </span>
-                                                    @endif
-                                                </td>
-                                                <td class="text-center">
-                                                    <a href="{{ route('userEdit', $item->id) }}" class="btn btn-sm btn-secondary">
-                                                        <i class="fas fa-edit"></i>
-                                                    </a>
-                                                    <button class="btn btn-sm btn-danger" data-toggle="modal" data-target="#exampleModal{{ $item->id }}">
-                                                        <i class="fas fa-trash"></i>
-                                                    </button>
-                                                    @include('admin/modal')
-                                                </td>
-                                            </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
-                                </div>
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead class="bg-danger text-white">
+                        <tr class="text-center">
+                            <th>No</th>
+                            <th>Nama</th>
+                            <th>Email</th>
+                            <th>Jabatan</th>
+                            <th>
+                                <i class="fas fa-cog"></i>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($user as $item)
+                        <tr>
+                            <td class="text-center">{{ $loop->iteration }}</td>
+                            <td class="text-center">{{ $item->nama }}</td>
+                            <td class="text-center">
+                                <span class="badge badge-secondary">
+                                    {{ $item->email }}
+                                </span>
+                            </td>
+                            <td class="text-center">
+                                @if ($item->Jabatan == 'Admin')
+                                <span class="badge badge-secondary">
+                                    {{ $item->Jabatan }}
+                                </span>
+                                @else
+                                <span class="badge badge-light">
+                                    {{ $item->Jabatan }}
+                                </span>
+                                @endif
+                            </td>
+                            <td class="text-center">
+                                <a href="{{ route('userEdit', $item->id) }}" class="btn btn-sm btn-secondary">
+                                    <i class="fas fa-edit"></i>
+                                </a>
+                                <button class="btn btn-sm btn-danger" data-toggle="modal" data-target="#exampleModal{{ $item->id }}">
+                                    <i class="fas fa-trash"></i>
+                                </button>
+                                @include('admin/modal')
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
     
