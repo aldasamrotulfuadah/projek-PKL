53cf1a9324b153e47024bae6c640a84cb1437bd6
aldasamrotulfuadah <alda.samrotul2007@smkn4-tsm.sch.id>
2026-04-06 14:18:12 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,10 @@
 
 @section('content')
 
-    <h1 class="h3 mb-4 text-gray-800">{{ $title }}</h1>
+    <h1 class="h3 mb-4 text-gray-800">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        {{ $title }}
+    </h1>
     <div class="row">
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-danger shadow h-100 py-2">
