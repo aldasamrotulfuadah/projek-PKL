1326bb6068411be949370822030c6d4f55b4fe26
aldasamrotulfuadah <alda.samrotul2007@smkn4-tsm.sch.id>
2026-04-07 10:07:50 +0700

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Masuk.in | Dashboard {{ $title }}</title>
+    <title>Masuk.in | {{ $title }}</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('sbadmin2/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
